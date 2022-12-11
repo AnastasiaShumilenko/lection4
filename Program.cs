1e872cbfa235c8fa2539f4d02dbcde9072eb1d5b
AnastasiaShumilenko <119425257+AnastasiaShumilenko@users.noreply.github.com>
2022-12-12 01:05:21 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+Console.Clear();
+
+void PrintArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+{
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ Console.Write($"{matr[i, j]} ");
+ }
+ Console.WriteLine();
+}
+}
+
+ void FillArray(int[,] matr)
+ {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+       {
+          matr[i,j] = new Random().Next(1,10); 
+       }
+      }
+     
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+    }
+}
+}
+}
